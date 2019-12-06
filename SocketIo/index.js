8d524a3aa6a857bae0b2d834adc8ebe0e5dfcf09
Linus Hören <linus.hoeren@veda.net>
2019-12-06 12:11:12 +0100

    Nicht mehr notwendige Zeile gelöscht, die aus vorherigen Aufgaben stammten.

--- SocketIo/index.js
+++ SocketIo/index.js
@@ -3,17 +3,7 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
-    res.sendfile('index.html')
-});
-
-io.on('connection', function (socket) {
-    socket.on('chat message', function (msg) {
-        console.log('message: ' + msg);
-    });
-});
-
-io.on('connection', function (socket) {
-    socket.broadcast.emit('hi');
+    res.sendfile(__dirname + '/index.html')
 });
 
 io.on('connection', function(socket){
