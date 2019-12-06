4ddf285800298bd4d9c80c6c0756d181cb157551
Linus Hören <linus.hoeren@veda.net>
2019-12-06 09:21:01 +0100

    h1-tag wurde aus index.js-Datei entfernt. An dieser Stelle wird eine HTML-Seite eingebunden (index-html).

--- SocketIo/index.js
+++ SocketIo/index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').createServer(app);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendfile('index.html')
 });
 
 http.listen(3000, function(){
