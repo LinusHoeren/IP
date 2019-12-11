706523ce70a89ad2e59466fb4a2328d728a8a40c
LinusHoeren <linus.hoeren@hotmail.com>
2019-12-11 15:07:34 +0100

    css-Datei ausgelagert.

--- SocketIo/index.js
+++ SocketIo/index.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html')
 });
